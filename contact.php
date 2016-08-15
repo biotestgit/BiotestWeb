6e0f9fbd32fa40493864a1aec3f2e7d64193f7ff
biotestgit <microbiology@biotestlab.com.au>
2016-08-15 17:05:31 +1000

    New template

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 		<!--[if lte IE 8]><script src="js/html5shiv.js" type="text/javascript"></script><![endif]-->
 		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 		<script src="js/jquery.dropotron.min.js"></script>		
-		<script src="js/skel.min.js">	
+		<script src="js/skel.min.js">
 		{
 			prefix: 'css/style',
 			preloadStyleSheets: true,
@@ -77,14 +77,14 @@
 					
 					<nav id="main-nav" class="8u">
 						<ul>
-							<li><a class="active" href="index.html">Home</a></li>
+							<li><a href="index.html">Home</a></li>
 							<li><a href="examples.html">Examples</a></li>
 							<li><a href="apage.html">A Page</a></li>
 							<li><a href="anotherpage.html">Another Page</a></li>							
 							<li>
 								<a href="#">Example Drop Down</a>			
 									<ul>
-										<li><a href="">Drop Down 1</a></li>
+										<li><a href="#">Drop Down 1</a></li>
 										<li><a href="#">Drop Down 2</a></li>
 										<li>
 											<a href="#">Drop Down 3</a>
@@ -98,16 +98,17 @@
 										</li>
 									</ul>
 							</li>
-							<li><a href="contact.php">Contact</a></li>
+							<li><a class="active" href="contact.php">Contact</a></li>
 						</ul>
 					</nav>
 				</div>
-			</div>	
-			
-        </div>		
-
-		<div id="site_content">
+			</div>		
 		
+		</div>		
+
+		<div id="site_content">		
+			<div class="container">	
+
 		    <div class="container">						
 				<div class="12u">
 					<div id="wrapper-banner">						
@@ -117,21 +118,11 @@
 						</div>	
 					</div>
 				</div>						
-			</div>		
-		
-		    <div class="container">						
-				<div class="12u">
-					<div id="strapline">						
-						<h2>Welcome to the CSS3_red_tulip template</h2>
-					</div>
-				</div>						
-			</div>		
-		
-			<div class="container">			
-			
-				<!-- Features -->			
-				<div class="row">									
-					
+			</div>				
+				
+			<!-- Features -->
+				<div class="row">
+
 					<section class="4u">
 						<div id="sidebar">
 							<section class="12u">
@@ -149,30 +140,76 @@
 									<li><a href="#">Last One</a></li>
 								</ul>
 							</section>
+							<section class="12u">
+								<h3>More Useful Links</h3>
+								<ul>
+									<li><a href="#">First Link</a></li>
+									<li><a href="#">Another Link</a></li>
+									<li><a href="#">And Another</a></li>
+									<li><a href="#">Last One</a></li>
+								</ul>
+							</section>
 						</div>
 					</section>
+					
+					<section class="8u">						
+						<h1>Contact Us</h1>
+						<p>Say hello, using this contact form.</p>
+						<?php
+							// Set-up these 3 parameters
+							// 1. Enter the email address you would like the enquiry sent to
+							// 2. Enter the subject of the email you will receive, when someone contacts you
+							// 3. Enter the text that you would like the user to see once they submit the contact form
+							$to = 'enter email address here';
+							$subject = 'Enquiry from the website';
+							$contact_submitted = 'Your message has been sent.';
 
-					<section class="8u">				
-						
-						<h3>Welcome to the CSS3_red_tulip template</h3>
-						<p>This simple, fixed width website template is released under a <a href="http://creativecommons.org/licenses/by/3.0">Creative Commons Attribution 3.0 Licence</a>. This means you are free to download and use it for personal and commercial projects. However, you <strong>must leave the 'design from css3templates.co.uk' link in the footer of the template</strong>.</p>
-						<p>This template is written entirely in <strong>HTML5</strong> and <strong>CSS3</strong>.</p>
-						<p>You can view more free CSS3 web templates <a href="http://www.css3templates.co.uk">here</a>.</p>
-						<p>This template is a fully documented 5 page website, with an <a href="examples.html">examples</a> page that gives examples of all the styles available with this design. There is also a working PHP contact form on the contact page.</p>
-						<h3>Browser Compatibility</h3>
-						<p>This template has been tested in the following browsers:</p>
-						<ul>
-							<li>Internet Explorer 9</li>
-							<li>FireFox 24</li>
-							<li>Google Chrome 31</li>
-						</ul>	
-						
-					</section>						
-
+							// Do not amend anything below here, unless you know PHP
+							function email_is_valid($email) {
+							  return preg_match('/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i',$email);
+							}
+							if (!email_is_valid($to)) {
+							  echo '<p style="color: red;">You must set-up a valid (to) email address before this contact page will work.</p>';
+							}
+							if (isset($_POST['contact_submitted'])) {
+								$return = "\r";
+								$youremail = trim(htmlspecialchars($_POST['your_email']));
+								$yourname = stripslashes(strip_tags($_POST['your_name']));
+								$yourmessage = stripslashes(strip_tags($_POST['your_message']));
+								$contact_name = "Name: ".$yourname;
+								$message_text = "Message: ".$yourmessage;
+								$user_answer = trim(htmlspecialchars($_POST['user_answer']));
+								$answer = trim(htmlspecialchars($_POST['answer']));
+								$message = $contact_name . $return . $message_text;
+								$headers = "From: ".$youremail;
+								if (email_is_valid($youremail) && !eregi("\r",$youremail) && !eregi("\n",$youremail) && $yourname != "" && $yourmessage != "" && substr(md5($user_answer),5,10) === $answer) {
+								  mail($to,$subject,$message,$headers);
+								  $yourname = '';
+								  $youremail = '';
+								  $yourmessage = '';
+								  echo '<p style="color: blue;">'.$contact_submitted.'</p>';
+								}
+								else echo '<p style="color: red;">Please enter your name, a valid email address, your message and the answer to the simple maths question before sending your message.</p>';
+							  }
+							  $number_1 = rand(1, 9);
+							  $number_2 = rand(1, 9);
+							  $answer = substr(md5($number_1+$number_2),5,10);
+						?>
+						<form id="contact" action="contact.php" method="post">
+						  <div class="form_settings">
+							<p><span>Name</span><input class="contact" type="text" name="your_name" value="<?php echo $yourname; ?>" /></p>
+							<p><span>Email Address</span><input class="contact" type="text" name="your_email" value="<?php echo $youremail; ?>" /></p>
+							<p><span>Message</span><textarea class="contact textarea" rows="5" cols="50" name="your_message"><?php echo $yourmessage; ?></textarea></p>
+							<p style="line-height: 1.7em;">To help prevent spam, please enter the answer to this question:</p>
+							<p><span><?php echo $number_1; ?> + <?php echo $number_2; ?> = ?</span><input type="text" name="user_answer" /><input type="hidden" name="answer" value="<?php echo $answer; ?>" /></p>
+							<p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" name="contact_submitted" value="send" /></p>
+						  </div>
+						</form>
+					</section>					
+					
 				</div>
-				
-			</div>
-        </div>	
+		    </div>
+		</div>			
 		
 		<div class="footer-wrapper">	
 			<div class="container">					
@@ -183,7 +220,6 @@
 					<p>Copyright &copy; CSS3_red_tulip | <a href="http://skeljs.org/">skel.js</a> | <a href="http://fotogrph.com/">Images</a> | <a href="http://www.css3templates.co.uk">design from css3templates.co.uk</a></p>
 				</footer>		
 			</div>
-		</div>
-			
+		</div>	
 	</body>
 </html>
